e2402469446f14816fbeddc0351ca1ece9e8aa59
LOKESHWAR-BH4G47 <bhagatlokeshwar7@proton.me>
2023-09-12 03:11:23 +0530

    added new interface

--- atm-system.py
+++ atm-system.py
@@ -1,126 +1,108 @@
-class User:
-    def __init__(self, user_id, pin):
-        self.user_id = user_id
-        self.pin = pin
-
-    def authenticate(self, entered_pin):
-        return self.pin == entered_pin
-
-
-class Account:
-    def __init__(self, account_number, balance):
+import random
+import sys
+ 
+class ATM():
+    def __init__(self, name, account_number, balance = 0):
+        self.name = name
         self.account_number = account_number
         self.balance = balance
-        self.transaction_history = TransactionHistory()  # Create a TransactionHistory instance
-
-    def check_balance(self):
-        return self.balance
-
-    def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            self.transaction_history.add_transaction_record(f"Withdrew ${amount}")
-            return True
-        else:
-            return False
-
+         
+    def account_detail(self):
+        print("\n----------ACCOUNT DETAIL----------")
+        print(f"Account Holder: {self.name.upper()}")
+        print(f"Account Number: {self.account_number}")
+        print(f"Available balance: Nu.{self.balance}\n")
+         
     def deposit(self, amount):
-        self.balance += amount
-        self.transaction_history.add_transaction_record(f"Deposited ${amount}")
-
-    def transfer(self, target_account, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            target_account.deposit(amount)
-            self.transaction_history.add_transaction_record(f"Transferred ${amount} to Account {target_account.account_number}")
-
-
-class ATM:
-    def __init__(self):
-        self.user = None
-        self.active = False
-
-    def start(self):
-        self.active = True
-
-    def main_menu(self):
-        while self.active:
-            print("1. Check Balance")
-            print("2. Withdraw Funds")
-            print("3. Deposit Funds")
-            print("4. Transfer Funds")
-            print("5. Transaction History")
-            print("6. Quit")
-            choice = input("Select an option: ")
-            self.process_user_input(choice)
-
-    def get_account_by_number(self, target_account_number):
-        accounts = [user1_account, user2_account, user3_account]  # Replace with actual account instances
-
-        for account in accounts:
-            if account.account_number == target_account_number:
-                return account
-
-        return None
-
-    def process_user_input(self, choice):
-        if choice == "1":
-            print("Balance:", self.user.account.check_balance())
-        elif choice == "2":
-            amount = float(input("Enter withdrawal amount: "))
-            if self.user.account.withdraw(amount):
-                print("Withdrawal successful.")
+        self.amount = amount
+        self.balance = self.balance + self.amount
+        print("Current account balance: Nu.", self.balance)
+        print()
+ 
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print("Insufficient fund!")
+            print(f"Your balance is Nu.{self.balance} only.")
+            print("Try with lesser amount than balance.")
+            print()
+        else:
+            self.balance = self.balance - self.amount
+            print(f"Nu.{amount} withdrawal successful!")
+            print("Current account balance: Nu.", self.balance)
+            print()
+ 
+    def check_balance(self):
+        print("Available balance: Nu.", self.balance)
+        print()
+ 
+    def transaction(self):
+        print("""
+            TRANSACTION 
+        *********************
+            Menu:
+            1. Account Detail
+            2. Check Balance
+            3. Deposit
+            4. Withdraw
+            5. Exit
+        *********************
+        """)
+        
+        while True:
+            try:
+                option = int(input("Enter 1, 2, 3, 4 or 5:"))
+            except:
+                print("Error: Enter 1, 2, 3, 4, or 5 only!\n")
+                continue
             else:
-                print("Insufficient funds.")
-        elif choice == "3":
-            amount = float(input("Enter deposit amount: "))
-            self.user.account.deposit(amount)
-            print("Deposit successful.")
-        elif choice == "4":
-            target_account_number = input("Enter target account number: ")
-            target_account = self.get_account_by_number(target_account_number)  # Use self to access the method
-            if target_account:
-                amount = float(input("Enter transfer amount: "))
-                self.user.account.transfer(target_account, amount)
-                print("Transfer successful.")
-            else:
-                print("Target account not found.")
-        elif choice == "5":
-            self.user.account.transaction_history.display_transaction_history()  # Access TransactionHistory
-        elif choice == "6":
-            self.quit()
-
-    def quit(self):
-        self.active = False
-        print("Thank you for using the ATM. Goodbye!")
-
-
-class TransactionHistory:
-    def __init__(self):
-        self.records = []
-
-    def add_transaction_record(self, record):
-        self.records.append(record)
-
-    def display_transaction_history(self):
-        for record in self.records:
-            print(record)
-
-
-# Example usage:
-user1 = User("12345", "1234")
-user1_account = Account("0001", 1000.0)
-user1.account = user1_account
-
-user2 = User("54321", "4321")
-user2_account = Account("0002", 1500.0)
-user2.account = user2_account
-
-user3 = User("98765", "5678")
-user3_account = Account("0003", 2000.0)
-user3.account = user3_account
-
-atm = ATM()
-atm.user = user1
-atm.start()
-atm.main_menu()
+                if option == 1:
+                    atm.account_detail()
+                elif option == 2:
+                    atm.check_balance()
+                elif option == 3:
+                    amount = int(input("How much you want to deposit(Nu.):"))
+                    atm.deposit(amount)
+                elif option == 4:
+                    amount = int(input("How much you want to withdraw(Nu.):"))
+                    atm.withdraw(amount)
+                elif option == 5:
+                    print(f"""
+                printing receipt..............
+          ******************************************
+              Transaction is now complete.                         
+              Transaction number: {random.randint(10000, 1000000)} 
+              Account holder: {self.name.upper()}                  
+              Account number: {self.account_number}                
+              Available balance: Nu.{self.balance}                    
+ 
+              Thanks for choosing us as your bank                  
+          ******************************************
+          """)
+                    sys.exit()
+                 
+ 
+print("*******WELCOME TO BANK OF BHUTAN*******")
+print("___________________________________________________________\n")
+print("----------ACCOUNT CREATION----------")
+name = input("Enter your name: ")
+account_number = input("Enter your account number: ")
+print("Congratulations! Account created successfully......\n")
+ 
+atm = ATM(name, account_number)
+ 
+while True:
+    trans = input("Do you want to do any transaction?(y/n):")
+    if trans == "y":
+        atm.transaction()
+    elif trans == "n":
+        print("""
+    -------------------------------------
+   | Thanks for choosing us as your bank |
+   | Visit us again!                     |
+    -------------------------------------
+        """)
+        break
+    else:
+        print("Wrong command!  Enter 'y' for yes and 'n' for NO.\n")
+    
